50f747e692476f000f3ce801c6c8dc745ca44c5e
Neo <neogoldahlgren@gmail.com>
2024-12-31 04:20:46 +0100

    markCells och resetMarkedCells
    Kodade till två nya funktioner till "sum" sidan.
    
    markCells markerar cellerna när man klickar på de och skriver ut summan i fältet.
    
    resetMarkedCells återställer cellerna och återställer summan i fältet.

--- sum/sum.js
+++ sum/sum.js
@@ -10,22 +10,38 @@ function getSumOfAllCells() {
 }
 
 function markCells(cell) {
-    
+    cell.classList.add("marked");
+    let cellValue = parseInt(cell.textContent);
+    markedCellsSum += cellValue;
+
+    sumOfMarked.value = markedCellsSum;
+}
+
+function resetMarked() {
+    removeMarked();
+    markedCellsSum = 0;
+    sumOfMarked.value = "-";
 }
 
 
 const sumOfAll = document.querySelector("#sum-of-all");
 const sumOfMarked = document.querySelector("#sum-of-marked");
 const reset = document.querySelector("#reset");
+sumOfMarked.value = "-";
+let markedCellsSum = 0;
 getSumOfAllCells();
 addEventListenersToCells("sum");
 
+reset.addEventListener("click", resetMarked);
+
 controls.buttonSelector.addEventListener("click", () => {
     getSumOfAllCells();
+    addEventListenersToCells("sum");
 });
 
 controls.inputSelector.addEventListener("keypress", (e) => {
     if (e.key === "Enter") {
         getSumOfAllCells();
+        addEventListenersToCells("sum");
     }
 });
